269c333c979c97d7f7a48a272748406decf0babf
Yoooo512 <yo@gmail.com>
2023-05-16 09:23:44 +0300

    мой первый камит

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, мир!");
+            Console.WriteLine("Работа с Git, VS");
+          	Console.WriteLine("эта строка дабавлена из Gogs");
+            Console.WriteLine("и это тоже строка дабавлена из Gogs");
+        }
+    }
+}
